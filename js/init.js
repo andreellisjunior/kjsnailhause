d8a66a10101e9dc04d58fa4446c020b7fbeb499f
Andre Ellis Jr <andreellisjunior@gmail.com>
2020-08-02 01:15:30 -0500

    first initial commit

---
+++ js/init.js
@@ -0,0 +1,22 @@
+(function ($) {
+  $(function () {
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.carousel').carousel({
+      duration: 500
+    });
+
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
