ea3814f1ca3242828cec90741a5750aaaee453c3
Andre Ellis Jr <andreellisjunior@gmail.com>
2021-02-24 23:09:22 -0600

    adding pwa round 5 feature to website

--- js/init.js
+++ js/init.js
@@ -26,3 +26,16 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// Detects if device is on iOS 
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  this.setState({ showInstallMessage: true });
+}
