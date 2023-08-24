bbb767945db67a25ab1aa9182fba815858b88292
blackdigitalgroup <andreellisjunior@gmail.com>
2023-08-24 17:58:43 -0500

    Updated policy

--- js/init.js
+++ js/init.js
@@ -1,45 +1,44 @@
 (function ($) {
   $(function () {
-
     $('.sidenav').sidenav({
-      edge: "right",
-      closeOnClick: true
+      edge: 'right',
+      closeOnClick: true,
     });
     $('.parallax').parallax();
     $('.carousel').carousel({
-      duration: 500
+      duration: 500,
     });
     $('.modal').modal();
-    var notice = document.querySelector(".bottom-sheet");
+    var notice = document.querySelector('.bottom-sheet');
     var instance = M.Modal.getInstance(notice);
     instance.open();
 
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
       anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
+          behavior: 'smooth',
         });
       });
     });
 
     // Prevents link from firing without confirmation
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-// Detects if device is on iOS 
+// Detects if device is on iOS
 const isIos = () => {
   const userAgent = window.navigator.userAgent.toLowerCase();
-  return /iphone|ipad|ipod/.test( userAgent );
-}
+  return /iphone|ipad|ipod/.test(userAgent);
+};
 // Detects if device is in standalone mode
-const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+const isInStandaloneMode = () =>
+  'standalone' in window.navigator && window.navigator.standalone;
 
 // Checks if should display install popup notification:
 if (isIos() && !isInStandaloneMode()) {
   const iosPopup = document.querySelector('#ios-popup');
 
-  iosPopup.style.display = "flex";
+  iosPopup.style.display = 'flex';
 }
